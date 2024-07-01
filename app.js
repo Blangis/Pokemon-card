d666d895b11f5fbe5b357de8b25aef27d972256e
Blangis <blancag.licona@gmail.com>
2024-06-30 22:38:58 -0600

    adding images to the card

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const random = getRandomInt(1,151);
-    fetchData(random);
-})
-
-const input = document.getElementById("space");
- const data = {
-    inputPoke: input.value
- }
-
 // Notesé que también en este caso `min` será incluido y `max` excluido
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
   }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const random = getRandomInt(1,151);
+    fetchData(random);
+});
+
+/*const input = document.getElementById("space");
+ const data = {
+    inputPoke: input.value
+ }
+*/
+
   
 
 console.log(getRandomInt(1, 151));
@@ -21,12 +24,29 @@ console.log(getRandomInt(1, 151));
 const fetchData = async (id) => {
     try {
         const res = await fetch (`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const data = await res.json()
-        console.log(data);
+        const data = await res.json();
+        
+        pintarCard(data);
 
 
     } catch (error) {
-        console.error(err);
+        console.error(error);
     }
+}
+
+const pintarCard = (pokemon) => {
+    console.log(pokemon);
+
+    const flex = document.querySelector(".flex");
+    const template = document.querySelector("#template-card").content;
+    const clone = template.cloneNode(true);
+    const fragment = document.createDocumentFragment();
+
+    clone.querySelector('.card-body-img').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
+
+    fragment.appendChild(clone);
+    flex.appendChild(fragment);
+
+
 }
 
